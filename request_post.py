2b15f52b244610b894af5a034a9f61648cb739e6
Prashant Singh <prashdash112@gmail.com>
2022-12-07 18:58:24 +0530

    adding procfile and request_post.py file

---
+++ request_post.py
@@ -0,0 +1,39 @@
+import requests
+
+inputdata = {'age': 29,
+
+'workclass': 'Private',
+
+'fnlgt': 12234,
+
+'education': 'Bachelors',
+
+'education_num': 12,
+
+'marital_status': 'Married_civ_spouse',
+
+'occupation': 'Prof_specialty',
+
+'relationship': 'Wife',
+
+'race': 'White',
+
+'sex': 'Female',
+
+'capital_gain': 0,
+
+'capital_loss': 0,
+
+'hours_per_week': 40,
+
+'native_country': 'Cuba'}
+
+response = requests.post(
+
+url='https://ml-dev-ops-salary-app.herokuapp.com/predict',
+
+json=inputdata)
+
+print(response.status_code)
+
+print(response.json())
