0d3735ad9882d51448d9bdd47f78366a07dea843
Prashant Singh <prashdash112@gmail.com>
2022-12-07 15:09:21 +0530

    fixed test_model.py

--- test_model.py
+++ test_model.py
@@ -48,24 +48,7 @@ def test_train_model():
     
     '''
     model = train_model(X_train,y_train)
-    assert model.get_params()=={'bootstrap': True,
-    'ccp_alpha': 0.0,
-    'class_weight': None,
-    'criterion': 'gini',
-    'max_depth': 5,
-    'max_features': 'auto',
-    'max_leaf_nodes': None,
-    'max_samples': None,
-    'min_impurity_decrease': 0.0,
-    'min_samples_leaf': 1,
-    'min_samples_split': 2,
-    'min_weight_fraction_leaf': 0.0,
-    'n_estimators': 100,
-    'n_jobs': None,
-    'oob_score': False,
-    'random_state': 101,
-    'verbose': 0,
-    'warm_start': False}
+    assert model.get_params()['max_depth'] == 5
     assert type(model) == RandomForestClassifier
 
 def test_inference():
